0edbe44afb4f9190178d6c34908ee35c2c3487f9
d09tri <hodangtri392001@gmail.com>
2022-04-12 08:12:46 +0700

    Tạo project GUI-DevExpress
    Giao diện chính giao tiếp với người dùng sử dụng công cụ DevExpress

---
+++ DA_PTPM_QLTV/GUI-DevExpress/XtraFrmMain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace GUI_DevExpress
+{
+    public partial class XtraFrmMain : DevExpress.XtraEditors.XtraForm
+    {
+        public XtraFrmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
