5c6dcee05512803185cb7d4bbec671c6aa16e129
d09tri <hodangtri392001@gmail.com>
2022-04-19 15:07:36 +0700

    Tạo project DTO

---
+++ DA_PTPM_QLTV/DTO/TheLoaiDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class TheLoaiDTO
+    {
+        public int MaTheLoai { get; set; }
+        public string TenTheLoai { get; set; }
+
+        public TheLoaiDTO() { }
+
+        public TheLoaiDTO(int maTheLoai, string tenTheLoai)
+        {
+            this.MaTheLoai = maTheLoai;
+            this.TenTheLoai = tenTheLoai;
+        }
+    }
+}
